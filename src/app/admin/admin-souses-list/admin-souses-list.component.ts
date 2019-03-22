a7a7dcd911a9c265ce14efe515ac9750eeab9968
Yuliya Voronovich <Y-1306@mail.ru>
2019-03-22 12:40:34 +0300

    update

---
+++ src/app/admin/admin-souses-list/admin-souses-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-souses-list',
+  templateUrl: './admin-souses-list.component.html',
+  styleUrls: ['./admin-souses-list.component.css']
+})
+export class AdminSousesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
